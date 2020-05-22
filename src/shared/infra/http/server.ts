e847a9fff4ee772a4f106dad98c19cafbe431716
melo_gustavo <gustavomelo95@gmail.com>
2020-05-22 15:36:27 -0300

    Add personal Error Handler

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,10 @@
+/* eslint-disable no-console */
 import 'reflect-metadata';
-import express from 'express';
-
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
+import AppError from '../../errors/AppError';
+
+import 'express-async-errors';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
@@ -11,7 +14,22 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
+
 app.listen(3333, () => {
-  // eslint-disable-next-line no-console
   console.log('Server is runing on port 3333');
 });
